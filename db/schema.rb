ccf52c04eb8f80277eb2be9c03f3c11ddb6e31db
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-01-09 15:41:13 -0800

    added paperclip but geting a PATCH error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108225850) do
+ActiveRecord::Schema.define(version: 20160109225457) do
 
   create_table "meetings", force: :cascade do |t|
     t.string   "subject"
@@ -28,8 +28,12 @@ ActiveRecord::Schema.define(version: 20160108225850) do
     t.string   "email"
     t.string   "type"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
